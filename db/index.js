47fdb0165598af162cc71e21c432cfd8d9089402
Terrence Ho <terrenceho.books@gmail.com>
2018-02-03 15:33:19 -0800

    Created Sequelize Database for Menu - Menu are complete now

--- db/index.js
+++ db/index.js
@@ -1,33 +1,36 @@
-const Models = require("./models");
+const Sequelize = require('sequelize');
 
-let Menu = Models.Menu;
+const logger = require('../logger');
+const error = require('../error');
+const config = require('../config');
 
-json_obj = {
-    test1: "value1",
-    test2: "value2"
-};
-let new_menu = new Menu({
-    name: "Awesome_Menu",
-    created_at: Date.now(),
-    updated_at: Date.now(),
-    menu: json_obj,
-});
-
-new_menu.Create(function(err) {
-    if (err) {
-        console.error(err);
+const db = new Sequelize(
+    config.database.name, 
+    config.database.user, 
+    config.database.password, 
+    {
+        dialect: config.database.dialect,
+        host: config.database.host,
+        logging: config.isDevelopment ? logger.debug : false,
     }
-    new_menu.Create( function(err) {
-        if (err) {
-            console.error(err);
-        }
-        new_menu.FindByName("Awesome_Menu", function(err, menus) {
-            if (err) {
-                console.error(err);
-            }
-            menus.forEach(function(menu) {
-                console.log(menu);
-            });
-        });
-    });
-});
+);
+
+const Menu = require('./MenuService.js')
+
+/**
+ * Handles database errors (separate from the general error handler and the 404 error handler)
+ *
+ * Specifically, it intercepts validation errors and presents them to the user in a readable
+ * manner. All other errors it lets fall through to the general error handler middleware.
+ */
+const errorHandler = (err, req, res, next) => {
+    if (!err || !(err instanceof Sequelize.Error))
+        return next(err);
+    if (err instanceof Sequelize.ValidationError) {
+        const message = `Validation Error: ${err.errors.map(e => e.message).join('; ')}`;
+        return next(new error.HTTPError(err.name, 422, message))
+    }
+    return next(new error.HTTPError(err.name, 500, err.message));
+};
+
+module.exports = { db, Menu, errorHandler };
