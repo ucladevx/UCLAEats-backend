cf23f6e0b6e956faa1ed8c94aa718844233785fe
Haozhuo <vichuanghaozhuo@outlook.com>
2018-02-03 17:02:44 -0800

    add test for scraper

---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+let scraper = require('./scraper.js');
+
+//console.log(scraper.getActivityLevel());
+//console.log(scraper.getOverviewPage('2018-02-03'));
+//console.log(scraper.getDetailPage('2018-02-02','Dinner'));
+//console.log(scraper.getDetailPage('2018-02-02','Breakfast'));
+//console.log(scraper.getDetailPage('2018-02-02','Lunch'));
+//console.log(scraper.getHours('2018-02-03'));
