e130be3c7a31441ce730c29a68d7f9270568c02f
Philip Chen <philipchen32@gmail.com>
2018-02-28 22:24:50 -0800

    created routes to nutritionfacts

--- api/v1/menu/index.js
+++ api/v1/menu/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
 const moment = require('moment');
 const OverviewMenu = require('../../../db').OverviewMenu;
@@ -6,8 +7,12 @@ const DetailedMenu = require('../../../db').DetailedMenu;
 const ActLevel = require('../../../db').ActLevel;
 const Hours = require('../../../db').Hours;
 
+const path = require("path");
+
+router.use(express.static(__dirname + '/public'));
+
 router.get('/nutritionfacts', (req, res) => {
-    res.render('nutritionfacts.html');
+    res.sendFile(path.join(__dirname + '/public/nutritionfacts.html'));
 });
 
 // Get overview menu from today til the next 7 days
