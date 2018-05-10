e0110964006cf60844bc90d4297a984e061c38ba
Haozhuo <vichuanghaozhuo@outlook.com>
2018-05-10 15:27:21 -0700

    add models

---
+++ scrape_virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/HaozhuoHuang/Desktop/clubs/UCLAEats-backend/scrape_virtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
