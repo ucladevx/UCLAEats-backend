c7e9cfd44d957d4cdce01172b1114fc273267353
Terrence Ho <terrenceho.books@gmail.com>
2018-05-22 15:41:03 -0700

    Deleted unnecessary files

--- scrape_virtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/HaozhuoHuang/Desktop/clubs/UCLAEats-backend/scrape_virtualenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
