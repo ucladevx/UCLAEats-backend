aed7156c84a074d7d025dee4df464110a1c28575
Terrence Ho <terrenceho.books@gmail.com>
2018-05-22 15:21:44 -0700

    Finished Scraper Config
    - Created new container for cron
    - Cron runs through django-crontab
    - Migrated Scraper to Alpine

--- menu_backend/entrypoint.sh
+++ menu_backend/entrypoint.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-/app/BruinBite/menu/manage.py crontab add
+# /app/BruinBite/menu/manage.py crontab add
 /app/BruinBite/menu/manage.py makemigrations scraper
 /app/BruinBite/menu/manage.py migrate
 /app/BruinBite/menu/manage.py test
 
 uwsgi --ini /app/BruinBite/menu/uwsgi.ini
+
+# /usr/sbin/crond -l 8
+
+exec "$@"
