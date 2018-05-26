0a53b8ea941f9487d8a8dd544cbff7b3bf55373b
Terrence Ho <terrenceho.books@gmail.com>
2018-05-25 19:57:41 -0700

    Editted entrypoint scripts

--- menu_backend/entrypoint-cron.sh
+++ menu_backend/entrypoint-cron.sh
@@ -2,8 +2,6 @@
 set -e
 
 /app/BruinBite/menu/manage.py crontab add
-/app/BruinBite/menu/manage.py makemigrations scraper
-/app/BruinBite/menu/manage.py migrate
 
 crond -f
 
--- menu_backend/entrypoint.sh
+++ menu_backend/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
-# /app/BruinBite/menu/manage.py crontab add
 /app/BruinBite/menu/manage.py makemigrations scraper
 /app/BruinBite/menu/manage.py migrate
 /app/BruinBite/menu/manage.py test --no-input
 
-uwsgi --ini /app/BruinBite/menu/uwsgi.ini
-
-# /usr/sbin/crond -l 8
+if [ $DJANGO_ENV == "prod" ]; then
+    uwsgi --ini /app/BruinBite/menu/uwsgi.ini
+else
+    /app/BruinBite/menu/manage.py runserver 0.0.0.0:8000
+fi
 
 exec "$@"
