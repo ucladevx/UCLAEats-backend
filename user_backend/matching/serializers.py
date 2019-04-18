604db21e9c7f256904f79a63428a9b18a459ac6c
shangyuhsu <shangyu.hsu@gmail.com>
2019-04-18 13:06:09 -0700

    Update datetime format for meal_times

--- user_backend/matching/serializers.py
+++ user_backend/matching/serializers.py
@@ -6,14 +6,19 @@ class WaitingUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = WaitingUser
         fields = ('id', 'user', 'meal_times', 'meal_day', 'meal_period', 
-                'dining_halls', 'status')
+                'dining_halls', 'status','date_updated')
 
     def create(self, validated_data):
+        def convert(t):
+            return t.strftime('%Y-%m-%d %H:%M:%S' )
+        validated_data['meal_times'] = list(map(convert, validated_data['meal_times']))
         return WaitingUser.objects.create(**validated_data)
 
     def update(self, waiting_user, **validated_data):
         waiting_user.dining_hall = validated.get("dining_hall",
                 waiting_user.dining_hall)
+
+   
         waiting_user.times = validated_data.get("times", waiting_user.times)
         waiting_user.meal_period = validated_data.get("meal_period",
                 waiting_user.meal_period)
@@ -28,6 +33,9 @@ class MatchedUsersSerializer(serializers.ModelSerializer):
             'meal_period', 'dining_hall', 'chat_url')
 
     def create(self, validated_data):
+        def convert(t):
+            return t.strftime('%Y-%m-%d %H:%M:%S' )
+        validated_data['meal_datetime'] = validated_data['meal_datetime'].strftime('%Y-%m-%d %H:%M:%S' )
         return MatchedUsers.objects.create(**validated_data)
 
     def update(self, matched_users, **validated_data):
