4d45a29d8b4165ec8a5b30d4ed6a3f44ab438e22
Shree Kesava Narayan Prasanna <kesavapr@gmail.com>
2019-05-05 18:54:18 -0700

    Removed invalid import

--- user_backend/users/views.py
+++ user_backend/users/views.py
@@ -10,7 +10,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from users.models import User
-from users.serializers import UserSerializer, ProfilePicSerializer
+from users.serializers import UserSerializer
 from users.S3Client import S3Client
 
 import base64
