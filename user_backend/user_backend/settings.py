671c76d3903b53dbb3a336659a7d81e5682863fc
franktzheng <31837071+franktzheng@users.noreply.github.com>
2019-11-03 16:40:46 -0800

    Password reset with Django PasswordResetView

--- user_backend/user_backend/settings.py
+++ user_backend/user_backend/settings.py
@@ -207,3 +207,10 @@ LOGGING = {
         },
     },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
